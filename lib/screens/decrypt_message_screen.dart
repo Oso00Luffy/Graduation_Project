4f5ae1d52ab2b39f510e6758064d2c4327bf62dd
Osama Jaradat <38565843+Oso00Luffy@users.noreply.github.com>
2025-04-25 15:36:53 +0300

    Update decrypt_message_screen.dart

--- lib/screens/decrypt_message_screen.dart
+++ lib/screens/decrypt_message_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; // Import for Clipboard functionality
 import '../services/encryption_service.dart';
 import '../widgets/custom_text_field.dart';
 import '../widgets/custom_button.dart';
@@ -19,7 +18,7 @@ class _DecryptMessageScreenState extends State<DecryptMessageScreen> {
     final key = _keyController.text;
 
     if (encryptedMessage.isNotEmpty && key.isNotEmpty) {
-      final decryptedMessage = EncryptionService.decryptMessage(encryptedMessage, key);
+      final decryptedMessage = EncryptionService.decryptAES(encryptedMessage, key);
       setState(() {
         _decryptedMessage = decryptedMessage;
       });
@@ -28,17 +27,6 @@ class _DecryptMessageScreenState extends State<DecryptMessageScreen> {
         _decryptedMessage = 'Please enter both an encrypted message and a key.';
       });
     }
-  }
-
-  void _copyToClipboard(String text) {
-    Clipboard.setData(ClipboardData(text: text));
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Copied to clipboard!')),
-    );
-  }
-
-  void _navigateToHome(BuildContext context) {
-    Navigator.of(context).popUntil((route) => route.isFirst); // Navigate back to Home
   }
 
   @override
@@ -52,47 +40,36 @@ class _DecryptMessageScreenState extends State<DecryptMessageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Decrypt Message'),
+        title: Text('Decrypt Message (AES)'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            CustomTextField(
-              controller: _messageController,
-              hintText: 'Enter the encrypted message',
-            ),
-            SizedBox(height: 16),
-            CustomTextField(
-              controller: _keyController,
-              hintText: 'Enter your key',
-            ),
-            SizedBox(height: 16),
-            CustomButton(
-              text: 'Decrypt',
-              onPressed: _decryptMessage,
-            ),
-            SizedBox(height: 16),
-            if (_decryptedMessage != null)
-              Column(
-                children: [
-                  Text(
-                    'Decrypted Message: $_decryptedMessage',
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 16),
-                  CustomButton(
-                    text: 'Copy to Clipboard',
-                    onPressed: () => _copyToClipboard(_decryptedMessage!),
-                  ),
-                  SizedBox(height: 16),
-                  CustomButton(
-                    text: 'Go to Home',
-                    onPressed: () => _navigateToHome(context),
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              CustomTextField(
+                controller: _messageController,
+                hintText: 'Enter the encrypted message',
               ),
-          ],
+              SizedBox(height: 16),
+              CustomTextField(
+                controller: _keyController,
+                hintText: 'Enter your AES key',
+              ),
+              SizedBox(height: 16),
+              CustomButton(
+                text: 'Decrypt',
+                onPressed: _decryptMessage,
+              ),
+              SizedBox(height: 16),
+              if (_decryptedMessage != null)
+                SelectableText(
+                  'Decrypted Message:\n$_decryptedMessage',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+            ],
+          ),
         ),
       ),
     );
