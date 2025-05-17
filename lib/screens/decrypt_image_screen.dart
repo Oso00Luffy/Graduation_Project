5fbb81b244999a3db9574b12ca0d9461282352f1
Oso00Luffy <osojr2017@gmail.com>
2025-05-17 23:09:23 +0300

    raising cap to 20mb

--- lib/screens/decrypt_image_screen.dart
+++ lib/screens/decrypt_image_screen.dart
@@ -24,7 +24,7 @@ class _DecryptImageScreenState extends State<DecryptImageScreen> {
   bool _isDecrypting = false;
   bool _showSuccess = false;
 
-  static const int MAX_IMAGE_SIZE = 5 * 1024 * 1024;
+  static const int MAX_IMAGE_SIZE = 20 * 1024 * 1024;
 
   Future<void> _pickImage(String purpose, Function(Uint8List) onImageSelected) async {
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
